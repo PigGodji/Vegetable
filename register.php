e2d24e9eb3909b1d6eed4c03addc233501547114
wor <157151293+Woradoii@users.noreply.github.com>
2024-01-24 12:35:16 +0700

    register.php

--- register.php
+++ register.php
@@ -1,9 +1,83 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <title></title>
-    </head>
-    <body>
-        <h1></h1>
-    </body>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Register</title>
+</head>
+<body>
+    <div class="top-bar">
+        <a href="#">About us</a>
+        <a href="index_reg.php">Plan</a>
+        <a href="index.html">Home</a>
+    </div>
+      <div class="container">
+        <div class="box form-box">
+
+        <?php 
+         
+         include("config.php");
+         if(isset($_POST['submit'])){
+            $username = $_POST['username'];
+            $email = $_POST['email'];
+            
+            $password = $_POST['password'];
+
+
+         $verify_query = mysqli_query($con,"SELECT Email FROM pbl_db WHERE Email='$email'");
+         if(mysqli_num_rows($verify_query) !=0 ){
+            echo "<div class='message'>
+                      <p>This email is used, Try another One Please!</p>
+                  </div> <br>";
+            echo "<a href='javascript:self.history.back()'><button class='btn'>Go Back</button>";
+         }
+         else{
+
+            mysqli_query($con,"INSERT INTO pbl_db(Username,Email,Password) VALUES('$username','$email','$password')") or die("Erroe Occured");
+
+            echo "<div class='message'>
+                      <p>Registration successfully!</p>
+                  </div> <br>";
+            echo "<a href='index_reg.php'><button class='btn'>Login Now</button>";
+         
+
+         }
+
+         }else{
+         
+        ?>
+
+            <header>Sign Up</header>
+            <form action="" method="post">
+                <div class="field input">
+                    <label for="username">Username</label>
+                    <input type="text" name="username" id="username" autocomplete="off" required>
+                </div>
+
+                <div class="field input">
+                    <label for="email">Email</label>
+                    <input type="text" name="email" id="email" autocomplete="off" required>
+                </div>
+
+                
+                <div class="field input">
+                    <label for="password">Password</label>
+                    <input type="password" name="password" id="password" autocomplete="off" required>
+                </div>
+
+                <div class="field">
+                    <input type="submit" class="btn" name="submit" value="Register" required>
+                </div>
+
+                <div class="links">
+                    Already a member? <a href="index_reg.php">Sign In</a>
+                </div>
+
+            </form>
+        </div>
+        <?php } ?>
+      </div>
+</body>
 </html>
